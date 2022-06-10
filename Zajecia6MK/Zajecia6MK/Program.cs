59ccccbdee3c2d399d83f31112ae9405d7f82d7d
Mateusz Kołacz <58779750+Matimateokol@users.noreply.github.com>
2022-06-10 16:31:39 +0200

    Add lesson 6

---
+++ Zajecia6MK/Zajecia6MK/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace Zajecia6MK
+{
+    class Program
+    {
+        static readonly string textFile = @"C:\Users\mateu\Documents\filesCreated\palindromes.txt";
+
+        private static string _wordFromStream { get; set; } 
+
+        private static bool isPalindrome(string word)
+        {
+            string rev;
+            char[] ch = word.ToCharArray();
+            Array.Reverse(ch);
+            rev = new string(ch);
+
+            return word.Equals(rev, StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static void readFile(string textFile)
+        {
+            if(File.Exists(textFile))
+            {
+                using(StreamReader file = new StreamReader(textFile))
+                {
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        _wordFromStream = ln;
+                        if(isPalindrome(_wordFromStream))
+                        {
+                            Console.WriteLine(_wordFromStream);
+                        }
+                    }
+                }
+            }
+        } 
+
+        static void Main(string[] args)
+        {
+            readFile(textFile);
+        }
+    }
+}
